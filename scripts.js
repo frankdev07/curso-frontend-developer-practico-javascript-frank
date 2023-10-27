1ddaf7bee0e5013291cc73ee370a8caac027acb1
Franklin Siliezar <frank.siliezar07@gmail.com>
2023-10-27 11:37:53 -0600

    Se agrega la funcion para mostrar y quitar el mobile menu

--- scripts.js
+++ scripts.js
@@ -2,10 +2,17 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', ToggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function ToggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+}
